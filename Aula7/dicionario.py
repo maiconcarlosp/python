46715acde02f602063107bae05634ae3d0c5443a
Maicon <maicon_mcp@hotmail.com>
2020-05-26 21:22:48 -0300

    Aula 7

---
+++ Aula7/dicionario.py
@@ -0,0 +1,7 @@
+cliente = {'nome':'Maicon',
+           'telefone':'41988251117',
+           'email':'maicon_mcp@hotmail.com'}
+
+cliente['endereco'] = 'Curitiba'
+
+print(cliente)
---
+++ Aula7/exercicioDicionario.py
@@ -0,0 +1,41 @@
+import os
+import statistics
+
+# pega o caminho da pasta através do caminho absoluto pois dependendo a versão pode não funcionar sem o abspath
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+alunos = {}
+matricula = 0
+
+while 1:
+     nome = input('Digite o nome do aluno: ')
+     if nome == '':
+          break
+
+     aluno = {'nome': nome}
+     notas = []
+
+     for i in range(3):
+          nota = input('Digite a nota {}: '.format(i+1))
+          notas.append(int(nota))
+
+     aluno['notas'] = notas
+
+     matricula += 1
+     alunos[matricula] = aluno
+
+relatorio = os.path.join(base_dir, 'relatorios')
+
+if len(alunos):
+     for matricula in alunos:
+          aluno = alunos[matricula]
+          arquivo = str(matricula) + '-' + aluno['nome'].replace(' ', '-') + '.txt'
+          boletim = open(os.path.join(relatorio, arquivo), 'w')
+
+          boletim.write('Nome: {}\n'.format(aluno['nome']))
+          for key, nota in enumerate(aluno['notas']):
+               boletim.write('Nota {}: {}\n'.format(key+1, nota))
+
+          boletim.write('Média: {}\n'.format(statistics.mean(aluno['notas'])))
+
+          boletim.close()
